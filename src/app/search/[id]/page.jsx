c4698fb7d442705a6f5bbaf99c8b25a7437c6864
devopsdbroe <dbroe1995@gmail.com>
2024-04-05 15:15:38 -0400

    Add search functionality

---
+++ src/app/search/[id]/page.jsx
@@ -0,0 +1,20 @@
+import Results from "@/components/Results";
+
+export default async function SearchPage({ params }) {
+	const searchTerm = params.id;
+
+	const res = await fetch(
+		`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en&page=1&include_adult=false`
+	);
+	const data = await res.json();
+	const results = data.results;
+	// console.log(results);
+	return (
+		<div>
+			{results && results.length === 0 && (
+				<h1 className="text-center pt-6">No results found</h1>
+			)}
+			{results && <Results results={results} />}
+		</div>
+	);
+}
